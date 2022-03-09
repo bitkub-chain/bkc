1848660c0923918c464f680b8926d09a55ee2581
mew.pawee <mew.pawee@bitkub.com>
2022-03-09 14:58:15 +0700

    change version

--- params/version.go
+++ params/version.go
@@ -22,14 +22,14 @@ import (
 
 const (
 	VersionMajor = 1        // Major version component of the current release
-	VersionMinor = 10       // Minor version component of the current release
-	VersionPatch = 16       // Patch version component of the current release
+	VersionMinor = 1       // Minor version component of the current release
+	VersionPatch = 0       // Patch version component of the current release
 	VersionMeta  = "stable" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
 var Version = func() string {
-	return fmt.Sprintf("%d.%d.%d", VersionMajor, VersionMinor, VersionPatch)
+	return fmt.Sprintf("%d.%d.%d-bkc", VersionMajor, VersionMinor, VersionPatch)
 }()
 
 // VersionWithMeta holds the textual version string including the metadata.
